39c0061f63a88759f610f68e40ad5fee73868469
NP-SON-250 <ishimweinnocent54@gmail.com>
2023-01-06 14:24:00 +0200

    portifolio updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,78 @@
-// alert("innocent");
-// window.addEventListener('load',()=>{
-
 const form = document.getElementById('form');
-const fname = document.getElementById('fname');
-const lname = document.getElementById('lname');
-const email = document.getElementById('email');
-const pass = document.getElementById('pass');
+    const user = document.getElementById('fname');;
+    const email = document.getElementById('email');
+    const mesg = document.getElementById('pass');
+    const lname = document.getElementById('lname');
 
 
-form.addEventListener('submit', e=>{
-e.preventDefault();
+    
+    
+    const setError =(el,sms) =>{
+        const inputControl = el.parentElement;
+        const errorDisplay = inputControl.querySelector(".error");
 
-checckInputs();
+        errorDisplay.innerText = sms;
+        errorDisplay.classList.add("error");
+        inputControl.classList.remove("success");
+    }
+    
+    const setSucc = Element =>{
+        const inputControl = Element.parentElement;
+        const errorDisplay = inputControl.querySelector(".error");
+        
+        if (errorDisplay != null) {
+            errorDisplay.innerText = " ";
+            errorDisplay.classList.remove("error");
+            inputControl.classList.add("success");
+        }
+        
+    }
+    const  checkEmail =(email)=> {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+    
+    const validatInputs = () =>{
+        const userValue = user.value.trim();
+        const emailValue = email.value.trim();
+        const msgValue = mesg.value.trim();
+        const lnameValue = lname.value.trim();
+        
+        if(userValue === ''){
+            setError(user, "First name is Required*");
+        }
+        else{
+            setSucc(user);
+        }
+        if(lnameValue === ''){
+            setError(lname, "Last name is Required*");
+        }
+        else{
+            setSucc(lname);
+        }
+        if(msgValue === ''){
+            setError(mesg, "Your message is Required*");
+        }
+        else{
+            setSucc(mesg);
 
-});
+        }
+        if(emailValue === ''){
+            setError(email, "Email is Required*");
+        }
+        else if(!checkEmail(emailValue)){
+            setError(email, "Provide valid Email Address*");
+        }
+        else{
+            setSucc(email);
+        }
+        
+        
+        
+    };
 
-const showError = (Element,msg) =>{
-const input = Element.parentElement;
-const show = input.querySelector('.error');
-
-show.innerText = msg;
-show.classList.add('error');
-input.classList.add('error')
-// input.classList.remove('success');
-show.classList.remove('success');
-}
-const showSucc = Element =>{
-const input = Element.parentElement;
-const show = input.querySelector('.error');
-
-show.innerText = ' ';
-show.classList.add('success');
-input.classList.add('success');
-show.classList.remove('error');
-input.classList.remove('error');
-}
-
-const checckInputs =()=>{
-const fnameV = fname.value.trim();
-const lnameV = lname.value.trim();
-const emailV = email.value.trim();
-const passV = pass.value.trim();
-
-
-if(fnameV === ''){
-    showError(fname, "Please Enter First Name!");
-}
-else{
-    showSucc(fname);
-}
-
-if(lnameV === ''){
-    showError(lname, "Please Enter Second Name!");
-}
-else{
-    showSucc(lname);
-}
-if(emailV === ''){
-    showError(email, "Please Enter Email!");
-}
-else{
-    showSucc(email);
-}
-if(passV === ''){
-    showError(pass, "Please Enter Message!");
-}
-else{
-    showSucc(pass);
-}
-}
-// });
+    form.addEventListener('submit', (e) =>{
+        e.preventDefault();
+    
+        validatInputs();
+    });
